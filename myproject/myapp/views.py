7de56527db2d0ab54f9b40e4703ae14038b17329
Ka Kwan Albert Lam <Albert@Kas-MacBook-Pro.local>
2017-08-06 22:19:31 +1000

    Update models.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from myapp.models import Dreamreal
 import datetime
 
 def hello(request):
@@ -23,3 +24,59 @@ def viewArticles(request, year, month):
 
 def morning(request):
 	return HttpResponse("""<h3>Good morning!</h3>""")
+
+def crudops(request):
+   #Creating an entry
+   
+   dreamreal = Dreamreal(
+      website = "www.albiewalblie.com", mail = "albie@albiewalbie.com", 
+      name = "albie", phonenumber = "002376970"
+   )
+   
+   dreamreal.save()
+   
+   #Read ALL entries
+   objects = Dreamreal.objects.all()
+   res ='Printing all Dreamreal entries in the DB : <br>'
+   
+   for elt in objects:
+      res += elt.name+"<br>"
+   
+   #Read a specific entry:
+   sorex = Dreamreal.objects.get(name = "sorex")
+   res += 'Printing One entry <br>'
+   res += sorex.name
+   
+   #Delete an entry
+   res += '<br> Deleting an entry <br>'
+   sorex.delete()
+   
+   #Update
+   dreamreal = Dreamreal(
+      website = "www.polo.com", mail = "sorex@polo.com", 
+      name = "sorex", phonenumber = "002376970"
+   )
+   
+   dreamreal.save()
+   res += 'Updating entry<br>'
+   
+   dreamreal = Dreamreal.objects.get(name = 'sorex')
+   dreamreal.name = 'thierry'
+   dreamreal.save()
+   
+   return HttpResponse(res)
+
+def datamanipulation(request):
+   res = ''
+   
+   #Filtering data:
+   qs = Dreamreal.objects.filter(name = "paul")
+   res += "Found : %s results<br>"%len(qs)
+   
+   #Ordering results
+   qs = Dreamreal.objects.order_by("name")
+   
+   for elt in qs:
+      res += elt.name + '<br>'
+   
+   return HttpResponse(res)
