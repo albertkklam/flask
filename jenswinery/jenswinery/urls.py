601eddb1a4cded2fa5af027a007e92553fc299b4
Albert Lam <albert233@hotmail.com>
2017-08-26 18:00:04 +1000

    Fun with Template Tags

--- jenswinery/jenswinery/urls.py
+++ jenswinery/jenswinery/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from wines import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^$', views.index, name='home'),
 ]
--- jenswinery/wines/views.py
+++ jenswinery/wines/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
-def index(request): 
-    return render(request, 'index.html')
+def index(request):
+    number = 6
+    thing = "Thing name"
+    return render(request, 'index.html', {'number': number, 'thing': thing})
