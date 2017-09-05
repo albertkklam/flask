ab9a1e0dd6f8dfe22977b0b6d34cd2e21d60577c
Albert Lam <albert233@hotmail.com>
2017-09-05 22:10:52 +1000

    Added extra fields to Wine model

--- jenswinery/wines/forms.py
+++ jenswinery/wines/forms.py
@@ -4,4 +4,4 @@ from wines.models import Wine, User
 class WineForm(ModelForm):
     class Meta:
         model = Wine
-        fields = ('name', 'description',)
+        fields = ('user','name','colour','year','grape','region_country','price','abv','description','mode_rating','winner',)
