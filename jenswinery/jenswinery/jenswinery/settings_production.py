457dbaf3b821e3117618182bbd039598aa8b48f8
Albert Lam <albert233@hotmail.com>
2017-09-23 18:26:27 +1000

    Updated staticfiles

--- jenswinery/jenswinery/jenswinery/settings_production.py
+++
@@ -1,8 +0,0 @@
-from jenswinery.settings import *
-import dj_database_url
-
-DATABASES['default'] =  dj_database_url.config()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
-DEBUG = False 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- jenswinery/jenswinery/urls.py
+++ jenswinery/jenswinery/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from wines.backends import MyRegistrationView
 from wines import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
