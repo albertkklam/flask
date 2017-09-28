6246c5015c614f8a7c5e4f159aad34faa53c3df6
Albert Lam <albert233@hotmail.com>
2017-09-28 21:33:05 +1000

    Updated settings.py

--- jenswinery/settings.py
+++ jenswinery/settings.py
@@ -125,7 +125,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # Django registration
