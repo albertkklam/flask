e0a66bc37abcd64b8d78fd9d9caea4d574959665
Albert Lam <albert233@hotmail.com>
2017-10-07 18:11:40 +1100

    Add empty style.css

--- jenswinery/settings.py
+++ jenswinery/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@+)c+ke4a=*#bq)(=qc@a9x93ny5m06d^i(@m0=wlu+5yybk)&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
